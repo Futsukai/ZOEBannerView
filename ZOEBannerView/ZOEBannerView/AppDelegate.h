40d1fc12794aa8a10bda884cfdb3261be373e92f
zhangwei <zhangwei@qiushicaifu.com>
2016-11-02 15:32:25 +0800

    add MIT
    MIT and Init

---
+++ ZOEBannerView/ZOEBannerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZOEBannerView
+//
+//  Created by zhangwei on 2016/11/2.
+//  Copyright © 2016年 Mr.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZOEBannerView/ZOEBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZOEBannerView
+//
+//  Created by zhangwei on 2016/11/2.
+//  Copyright © 2016年 Mr.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
